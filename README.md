fa062cf72e95045bddb07a47a78b134954bdcabd
GadgetAngel <joannmanges@gmail.com>
2019-04-26 04:22:11 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,287 @@
+# tuyAPI-MQTT Client
+MQTT interface for Tuya home automation devices sold under various names.
+This is a wrapper script for the Project codetheweb/tuyapi. https://github.com/codetheweb/tuyapi
+
+This project provides an MQTT client for communication with the home automation devices.
+
+:exclamation: There is a greate Step-By-Step guide from user HolgiHab at openhab community ([Step-By-Step Guide](
+https://community.openhab.org/t/step-by-step-guide-for-adding-tuya-bulbs-smart-life-to-oh2-using-tuya-mqtt-js-by-agentk/59371)). This guide is not only for light bulbs, but also applies to sockets. :exclamation:
+
+## Instructions:
+
+Download this project to your openhab2-script-folder "/etc/openhab2/scripts" and install tuyapi from the same folder that the tuya-mqtt.js is in
+```
+cd /etc/openhab2/scripts
+
+// clone this project
+git clone git@github.com:TheAgentK/tuya-mqtt.git
+
+// change directory to the project directory
+cd tuya-mqtt
+
+//installs this project along with codetheweb/tuyapi project
+npm install
+```
+
+See the setup instructions found here: https://github.com/codetheweb/tuyapi/blob/master/docs/SETUP.md
+
+
+## Basic Usage
+
+### Create your configuration file:
+```
+cp config.json.sample config.json
+
+// edit the configuration file
+nano config.json 
+```
+
+### Start command
+```
+node tuya-mqtt.js
+
+// For debugging purpose, to use DEBUG : https://www.npmjs.com/package/debug
+
+//on Linux machines at the bash command prompt, to turn ON DEBUG:
+DEBUG=* tuya-mqtt.js
+
+//on Linux machines at the bash command prompt, to turn OFF DEBUG:
+DEBUG=-* tuya-mqtt.js
+
+// on Windows machines at the cmd.exe command prompt, to turn ON DEBUG:
+Set DEBUG=* & node c:/openhab2/userdata/etc/scripts/tuya-mqtt.js
+
+// on Windows machines at the cmd.exe command prompt, to turn OFF DEBUG:
+Set DEBUG=-* & node c:/openhab2/userdata/etc/scripts/tuya-mqtt.js
+```
+URL to install [DEBUG](https://www.npmjs.com/package/debug)
+
+
+
+### MQTT Topic's (send data)
+```
+Change device state (by topic):
+    tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/<STATE>
+
+    Example:
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/on
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/off
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/ON
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/OFF
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/1
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/0
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/toggle
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/TOGGLE
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/{ "dps": 1, "set": true }
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/{ "dps": 7, "set": true }
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/{ "multiple": true, "data": { "1": true, "7": true } }
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/{ "schema": true }
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/{ "multiple": true, "data": { "1": true, "2": "scene_4" } }
+    - tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/{ "multiple": true, "data": { "1": true, "2": "scene", "6": "c479000025ffc3" } } 
+
+Change device state (by payload)
+Use with OpenHAB 2.X MQTT bindings or others where only a single command topic is preferred:
+NOTE: notice that nothing follows the word command, DO NOT but a "/" in after command.
+
+    tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command
+
+    Example:
+    "ON"
+    "OFF"
+    "on"
+    "off"
+    "1"
+    "0"
+    "toggle"
+    "TOGGLE"
+    "{ \"dps\": 1, \"set\": true }"
+    "{ \"dps\": 7, \"set\": true }"
+    "{ \"multiple\": true, \"data\": { \"1\": true, \"7\": true } }"
+    "{ \"schema\": true }"
+    "{ \"multiple\": true, \"data\": { \"1\": true, \"2\": \"scene_4\" } }"
+    "{ \"multiple\": true, \"data\": { \"1\": true, \"2\": \"scene\", \"6\": \"c479000025ffc3\" } }"
+
+Change color of lightbulb (payload as HSB-Color)
+    tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/color
+
+    Example:
+    64,0,100
+    0,0,89
+```
+
+### MQTT Topic's (read data)
+```
+Current device state (allways DPS[1]-Value):
+    tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/state
+
+Device DPS-Values:
+    // returns JSON.stringify(dps) values, use with care, does not always contain all dps values
+    tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/dps
+
+    // return single dps data value
+    tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/dps/<tuya-dps-id>
+```
+
+## Issues
+There are some reliability issues with tuyapi. Latest changes changed the syntax but still getting error maybe at an even higher rate.
+
+All questions regarding the tuyAPI please ask in the project https://github.com/codetheweb/tuyapi .
+
+
+## Example items for OpenHAB 1.x Bindings (still works with OH > 2.4 but only if legacy 1.x MQTT bindings are enabled)
+### simple switch on/off
+```
+
+Switch tuya_kitchen_coffeemachine_mqtt "Steckdose Kaffeemaschine" <socket> (<GROUPS>) ["Switchable"] {
+    mqtt="<[broker:tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/state:state:default:.*],
+          >[broker:tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/on:command:ON:true],
+          >[broker:tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/off:command:OFF:false]"
+}
+
+Switch tuya_livingroom_ledstrip_tv "LED Regal" <lightbulb> (<GROUPS>) ["Lighting"] {
+    mqtt="<[broker:tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/state:state:default:.*],
+          >[broker:tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/on:command:ON:true],
+          >[broker:tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command/off:command:OFF:false]"
+}
+
+```
+
+### change color of lightbulb
+```
+
+# .items
+Group gTuyaLivingColor "Tuya color group" <lightbulb>
+Color tuya_livingroom_colorpicker "Stehlampe farbe" (LivingDining)
+
+String tuya_livingroom_ledstrip_tv_color "Set color [%s]" (gTuyaLivingColor, LivingDining) {
+    mqtt=">[broker:tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/color:command:*:default]"
+}
+
+
+
+# .rules
+import org.openhab.core.library.types.HSBType;
+
+rule "Set HSB value of item RGBLed to RGB color value"
+    when
+        Item tuya_livingroom_colorpicker received command
+    then
+        var appName = "Colorpicker.livingroom"
+        var color = receivedCommand.toString;
+
+        // get all colors and send it via mqtt if light ist enabled
+        gTuyaLivingColor.members.forEach[ i |
+            var name = i.name;
+            var stateName = name.toString.split("_color").get(0);
+            var stateItem = gTuyaLights.allMembers.filter [ conf | conf.name.contains(stateName.toString) ].head;
+
+            if(stateItem.state == ON){
+                logInfo(appName, name + " change to color: " + color);
+                i.sendCommand(color);
+                Thread::sleep(400);
+            }
+        ]
+    end
+
+```
+
+## Example items for OpenHAB 2.4 Bindings
+### simple switch on/off
+
+With OpenHAB 2.X MQTT bindings you can add devices using a generic MQTT Thing via PaperUI or 
+configuration files.  For PaperUI simply at the generic MQTT Thing and set the state and
+command topics as follows:
+```
+
+    tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/state
+
+    tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command
+
+```
+
+If you prefer using configuration files vs PaperUI, it should look something like this:
+See also OpenHAB 2.X MQTT binding [documentation](https://www.openhab.org/v2.4/addons/bindings/mqtt.generic/)
+
+```
+
+Bridge mqtt:broker:myUnsecureBroker [ host="localhost", secure=false ]
+{
+
+    Thing mqtt:topic:myCustomMQTT {
+    Channels:
+        Type switch : tuya_kitchen_coffeemachine_mqtt_channel "Kitchen Coffee Machine MQTT Channel" [
+            stateTopic="tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/state",
+            commandTopic="tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/command",
+
+            // optional custom mqtt-payloads for ON and OFF
+            on="{ \"dps\": 1, \"set\": true },
+            off="0"
+        ]
+    }
+
+}
+
+# *.item Example
+Switch tuya_kitchen_coffeemachine_mqtt "Kitchen Coffee Machine Switch" <socket> (gKitchen, gTuya) ["Switchable"] {
+    channel="mqtt:topic:myUnsecureBroker:myCustomMQTT:tuya_kitchen_coffeemachine_mqtt_channel"
+}
+
+```
+
+For one RGB bulb you would need a separate channel with the command topic set to
+`tuya/<tuyAPI-id>/<tuyAPI-key>/<tuyAPI-ip>/color` and link that to your color item.
+
+```
+
+Bridge mqtt:broker:myUnsecureBroker [ host="localhost", secure=false ]
+{
+    Thing mqtt:topic:myCustomMQTT {
+    Channels:
+        Type colorHSB : livingroom_floorlamp_1_color "Livingroom floorlamp color MQTT Channel" [
+            stateTopic="tuya/05200399bcddc2e02ec9/b58cf92e8bc5c899/192.168.178.49/state",
+            commandTopic="tuya/05200399bcddc2e02ec9/b58cf92e8bc5c899/192.168.178.49/color"
+        ]
+    }
+}
+
+# *.item Example
+Color tuya_livingroom_colorpicker "Floorlamp colorpicker" (gLivingroom){
+    channel="mqtt:topic:myUnsecureBroker:myCustomMQTT:livingroom_floorlamp_1_color"
+}
+
+```
+
+#### Basic UI sitemap
+```
+
+Switch item=tuya_kitchen_coffeemachine_mqtt
+
+# turn the color bulb off or on
+Switch item=tuya_livingroom_colorpicker label="RGB lamp [%s]" 
+
+# pick the color level to send to the color bulb via MQTT color Channel
+Slider item=tuya_livingroom_colorpicker label="RGB lamp level [%s]" minValue=0 maxValue=100 step=1
+
+# color picked and sent via MQTT Color channel
+Colorpicker item=tuya_livingroom_colorpicker label="RGB lamp color [%s]" icon="colorpicker" sendFrequency=30000
+
+
+```
+
+## Contributors
+- [TheAgentK](https://github.com/TheAgentK)
+- [tsightler](https://github.com/tsightler)
+- [Tycale](https://github.com/Tycale)
+- [crashdummymch](https://github.com/crashdummymch)
+- [GadgetAngel](https://github.com/GadgetAngel)
+
+
+## Related Projects:
+- https://github.com/codetheweb/tuyapi
+- https://github.com/unparagoned/njsTuya
+- https://github.com/clach04/python-tuya
+- https://github.com/Marcus-L/m4rcus.TuyaCore
+- Specs: https://docs.tuya.com/en/cloudapi/cloud_access.html
+
+[![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com)
---
+++ tuya-mqtt.js
@@ -0,0 +1,374 @@
+const mqtt = require('mqtt');
+const TuyaDevice = require('./tuya-device');
+const debug = require('debug')('TuyAPI:mqtt');
+const debugColor = require('debug')('TuyAPI:mqtt:color');
+const debugTuya = require('debug')('TuyAPI:mqtt:device');
+const debugError = require('debug')('TuyAPI:mqtt:error');
+var cleanup = require('./cleanup').Cleanup(onExit);
+
+function bmap(istate) {
+    return istate ? 'ON' : "OFF";
+}
+
+function boolToString(istate) {
+    return istate ? 'true' : "false";
+}
+
+var connected = undefined;
+var CONFIG = undefined;
+
+try {
+    CONFIG = require("./config");
+} catch (e) {
+    console.error("Configuration file not found")
+    debugError(e)
+    process.exit(1)
+}
+
+if (typeof CONFIG.qos == "undefined") {
+    CONFIG.qos = 2;
+}
+if (typeof CONFIG.retain == "undefined") {
+    CONFIG.retain = false;
+}
+
+const mqtt_client = mqtt.connect({
+    host: CONFIG.host,
+    port: CONFIG.port,
+    username: CONFIG.mqtt_user,
+    password: CONFIG.mqtt_pass,
+});
+
+mqtt_client.on('connect', function (err) {
+    debug("Verbindung mit MQTT-Server hergestellt");
+    connected = true;
+    var topic = CONFIG.topic + '#';
+    mqtt_client.subscribe(topic, {
+        retain: CONFIG.retain,
+        qos: CONFIG.qos
+    });
+});
+
+mqtt_client.on("reconnect", function (error) {
+    if (connected) {
+        debug("Verbindung mit MQTT-Server wurde unterbrochen. Erneuter Verbindungsversuch!");
+    } else {
+        debug("Verbindung mit MQTT-Server konnte nicht herrgestellt werden.");
+    }
+    connected = false;
+});
+
+mqtt_client.on("error", function (error) {
+    debug("Verbindung mit MQTT-Server konnte nicht herrgestellt werden.", error);
+    connected = false;
+});
+
+/**
+ * execute function on topic message
+ */
+
+function IsJsonString(text) {
+    if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
+        //the json is ok
+        return true;
+    }
+    return false;
+}
+
+/**
+ * check mqtt-topic string for old notation with included device type
+ * @param {String} topic
+ */
+function checkTopicForOldNotation(_topic) {
+    var topic = _topic.split("/");
+    var type = topic[1];
+    var result = (type == "socket" || type == "lightbulb");
+    return result;
+}
+
+/**
+ * get action from mqtt-topic string
+ * @param {String} topic
+ * @returns {String} action type
+ */
+function getActionFromTopic(_topic) {
+    var topic = _topic.split("/");
+
+    if (checkTopicForOldNotation(_topic)) {
+        return topic[5];
+    } else {
+        return topic[4];
+    }
+}
+
+/**
+ * get device informations from mqtt-topic string
+ * @param {String} topic
+ * @returns {String} object.id
+ * @returns {String} object.key
+ * @returns {String} object.ip
+ */
+function getDeviceFromTopic(_topic) {
+    var topic = _topic.split("/");
+
+    if (checkTopicForOldNotation(_topic)) {
+        return {
+            id: topic[2],
+            key: topic[3],
+            ip: topic[4],
+            type: topic[1]
+        };
+    } else {
+        return {
+            id: topic[1],
+            key: topic[2],
+            ip: topic[3]
+        };
+    }
+}
+
+/**
+ * get command from mqtt - topic string
+ * converts simple commands to TuyAPI JSON commands
+ * @param {String} topic
+ * @returns {Object}
+ */
+function getCommandFromTopic(_topic, _message) {
+    var topic = _topic.split("/");
+    var command = null;
+
+    if (checkTopicForOldNotation(_topic)) {
+        command = topic[6];
+    } else {
+        command = topic[5];
+    }
+
+    if (command == null) {
+        command = _message;
+    }
+
+    if (command != "1" && command != "0" && IsJsonString(command)) {
+        debug("command is JSON");
+        command = JSON.parse(command);
+    } else {
+        if (command.toLowerCase() != "toggle") {
+            // convert simple commands (on, off, 1, 0) to TuyAPI-Commands
+            var convertString = command.toLowerCase() == "on" || command == "1" || command == 1 ? true : false;
+            command = {
+                set: convertString
+            }
+        } else {
+            command = command.toLowerCase();
+        }
+    }
+
+    return command;
+}
+
+mqtt_client.on('message', function (topic, message) {
+    try {
+        message = message.toString();
+        var action = getActionFromTopic(topic);
+        var options = getDeviceFromTopic(topic);
+
+        debug("receive settings", JSON.stringify({
+            topic: topic,
+            action: action,
+            message: message,
+            options: options
+        }));
+
+        var device = new TuyaDevice(options);
+        device.then(function (params) {
+            var device = params.device;
+
+            switch (action) {
+                case "command":
+                    var command = getCommandFromTopic(topic, message);
+                    debug("receive command", command);
+                    if (command == "toggle") {
+                        device.switch(command).then((data) => {
+                            debug("set device status completed", data);
+                        });
+                    }
+                    if (command.schema === true) {
+                        // this command is very useful. IT IS A COMMAND. It's place under the command topic.
+                        // It's the ONLY command that does not use device.set to get a result.
+                        // You have to use device.get and send the get method an exact JSON string of { schema: true }
+                        // This schema command does NOT
+                        // change the state of the device, all it does is query the device
+                        // as a confirmation that all communications are working properly.
+                        // Otherwise you have to physically change the state of the device just to
+                        // find out if you can talk to it.  If this command returns no errors than
+                        // we know we are have an established communication channel.  This is a native TuyAPI call that
+                        // the TuyAPI interface defines (its only available via the GET command.
+                        // this call returns a object of results
+                        device.schema(command).then((data) => {
+                        });
+                        debug("get (schema) device status completed");
+                    } else {
+                        device.set(command).then((data) => {
+                            debug("set device status completed", data);
+                        });
+                    }
+                    break;
+                case "color":
+                    var color = message.toLowerCase();
+                    debugColor("set color: ", color);
+                    device.setColor(color).then((data) => {
+                        debug("set device color completed", data);
+                    });
+                    break;
+            }
+
+        }).catch((err) => {
+            debugError(err);
+        });
+    } catch (e) {
+        debugError(e);
+    }
+});
+
+/**
+ * Publish current TuyaDevice state to MQTT-Topic
+ * @param {TuyaDevice} device
+ * @param {boolean} status
+ */
+function publishStatus(device, status) {
+    if (mqtt_client.connected == true) {
+        try {
+            var type = device.type;
+            var tuyaID = device.options.id;
+            var tuyaKey = device.options.key;
+            var tuyaIP = device.options.ip;
+
+            if (typeof tuyaID != "undefined" && typeof tuyaKey != "undefined" && typeof tuyaIP != "undefined") {
+                var topic = CONFIG.topic;
+                if (typeof type != "undefined") {
+                    topic += type + "/";
+                }
+                topic += tuyaID + "/" + tuyaKey + "/" + tuyaIP + "/state";
+
+                mqtt_client.publish(topic, status, {
+                    retain: CONFIG.retain,
+                    qos: CONFIG.qos
+                });
+                debugTuya("mqtt status updated to:" + topic + " -> " + status);
+            } else {
+                debugTuya("mqtt status not updated");
+            }
+        } catch (e) {
+            debugError(e);
+        }
+    }
+}
+
+function publishColorState(device, state) {
+
+}
+
+/**
+ * publish all dps-values to topic
+ * @param  {TuyaDevice} device
+ * @param  {Object} dps
+ */
+function publishDPS(device, dps) {
+    if (mqtt_client.connected == true) {
+        try {
+            var type = device.type;
+            var tuyaID = device.options.id;
+            var tuyaKey = device.options.key;
+            var tuyaIP = device.options.ip;
+
+            if (typeof tuyaID != "undefined" && typeof tuyaKey != "undefined" && typeof tuyaIP != "undefined") {
+                var baseTopic = CONFIG.topic;
+                if (typeof type != "undefined") {
+                    baseTopic += type + "/";
+                }
+                baseTopic += tuyaID + "/" + tuyaKey + "/" + tuyaIP + "/dps";
+
+                var topic = baseTopic;
+                var data = JSON.stringify(dps);
+                debugTuya("mqtt dps updated to:" + topic + " -> ", data);
+                mqtt_client.publish(topic, data, {
+                    retain: CONFIG.retain,
+                    qos: CONFIG.qos
+                });
+
+                Object.keys(dps).forEach(function (key) {
+                    var topic = baseTopic + "/" + key;
+                    var data = JSON.stringify(dps[key]);
+                    debugTuya("mqtt dps updated to:" + topic + " -> dps[" + key + "]", data);
+                    mqtt_client.publish(topic, data, {
+                        retain: CONFIG.retain,
+                        qos: CONFIG.qos
+                    });
+                });
+            } else {
+                debugTuya("mqtt dps not updated");
+            }
+        } catch (e) {
+            debugError(e);
+        }
+    }
+}
+
+/**
+ * event fires if TuyaDevice sends data
+ * @see TuyAPI (https://github.com/codetheweb/tuyapi)
+ */
+TuyaDevice.onAll('data', function (data) {
+    try {
+        if (typeof data.dps != "undefined") {
+            debugTuya('Data from device ' + this.type + ' :', data);
+            var status = data.dps['1'];
+            if (typeof status != "undefined") {
+                publishStatus(this, bmap(status));
+            }
+            publishDPS(this, data.dps);
+        }
+    } catch (e) {
+        debugError(e);
+    }
+});
+
+/**
+ * MQTT connection tester
+ */
+function MQTT_Tester() {
+    this.interval = null;
+
+    function mqttConnectionTest() {
+        if (mqtt_client.connected != connected) {
+            connected = mqtt_client.connected;
+            if (connected) {
+                debug('MQTT-Server verbunden.');
+            } else {
+                debug('MQTT-Server nicht verbunden.');
+            }
+        }
+    }
+
+    this.destroy = function () {
+        clearInterval(this.interval);
+        this.interval = undefined;
+    }
+
+    this.connect = function () {
+        this.interval = setInterval(mqttConnectionTest, 1500);
+        mqttConnectionTest();
+    }
+
+    var constructor = (function (that) {
+        that.connect.call(that);
+    })(this);
+}
+var tester = new MQTT_Tester();
+
+/**
+ * Function call on script exit
+ */
+function onExit() {
+    TuyaDevice.disconnectAll();
+    if (tester) tester.destroy();
+};
